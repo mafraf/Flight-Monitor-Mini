ddaa4098c2a321e81279f8b6daf669a4aea76e63
Ondra Vagner <VagnerOndra@gmail.com>
2012-05-16 23:29:39 +0200

    Snad teď

--- GJAProject/src/cz/vutbr/fit/gja/proj/GraphingData.java
+++ GJAProject/src/cz/vutbr/fit/gja/proj/GraphingData.java
@@ -4,35 +4,35 @@ import java.awt.font.*;
 import java.awt.geom.*;
 import javax.swing.*;
 import java.awt.*;
-import java.util.*; 
+import java.util.*;
 import cz.vutbr.fit.gja.proj.utils.*;
- 
+
 public class GraphingData extends JPanel implements PanelInterface
 {
     ArrayList<Integer> gdata;
     final int PAD = 20;
     final int down = 80;
- 
+
     public GraphingData()
     {
-      super();   
+      super();
       gdata = new ArrayList<Integer>();
       Dimension d = new Dimension(1000, 200);
       this.setMinimumSize(d);
       this.setMaximumSize(d);
       this.setPreferredSize(d);
     }
-    
+
     public void setData(TelemetryData.TelemetryItem item)
     {
-    
+
     }
-    
+
     public void changeSpeed(double max)
     {
-    
+
     }
-    
+
     public void setAllData(TelemetryData.TelemetryVar data, int max)
     {
       gdata.clear();
@@ -42,16 +42,16 @@ public class GraphingData extends JPanel implements PanelInterface
       }
       revalidate();
     }
-    
+
     @Override
-    public void paintComponent(Graphics g) 
+    public void paintComponent(Graphics g)
     {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D)g;
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
                             RenderingHints.VALUE_ANTIALIAS_ON);
         int w = getWidth();
-        int h = getHeight(); 
+        int h = getHeight();
         g2.draw(new Line2D.Double(PAD, PAD, PAD, h-PAD));
         g2.draw(new Line2D.Double(PAD, h-PAD-down, w-PAD, h-PAD-down));
         // Draw labels.
@@ -64,18 +64,18 @@ public class GraphingData extends JPanel implements PanelInterface
         String s = "0";
         float sy = PAD + ((h - 2*PAD) - s.length()*sh)/2 + lm.getAscent();
         float sw = (float)font.getStringBounds(s, frc).getWidth();
-        float sx = (PAD - sw)/2;        
+        float sx = (PAD - sw)/2;
         g2.drawString(s, sx, sy);
         int max = getMax();
         if(!gdata.isEmpty())
         {
           s = Integer.toString(max);
           sy = PAD - PAD/4;
-          sx = (PAD - sw)/2; 
+          sx = (PAD - sw)/2;
           g2.drawString(s, sx, sy);
           s = Integer.toString(-1*max);
           sy = h - PAD/4;
-          g2.drawString(s, sx, sy);       
+          g2.drawString(s, sx, sy);
         }
         // Abcissa label.
         s = "Čas";
@@ -87,24 +87,24 @@ public class GraphingData extends JPanel implements PanelInterface
         double xInc = (double)(w - 2*PAD)/((double)gdata.size()-1);
         double scale = (double)(h - down - 2*PAD)/max;
         g2.setPaint(Color.green.darker());
-        for(int i = 0; i < gdata.size()-1; i++) 
+        for(int i = 0; i < gdata.size()-1; i++)
         {
             double x1 = PAD + i*xInc;
-            double y1 = h - down - PAD - scale*((double)gdata.get(i)); 
+            double y1 = h - down - PAD - scale*((double)gdata.get(i));
             double x2 = PAD + (i+1)*xInc;
             double y2 = h - down - PAD - scale*((double)gdata.get(i+1));
             g2.draw(new Line2D.Double(x1, y1, x2, y2));
         }
         // Mark data points.
        /* g2.setPaint(Color.red);
-        for(int i = 0; i < gdata.size(); i++) 
+        for(int i = 0; i < gdata.size(); i++)
         {
             double x = PAD + i*xInc;
             double y = h - down - PAD - scale*((double)gdata.get(i));
             g2.fill(new Ellipse2D.Double(x-2, y-2, 4, 4));
         }*/
     }
- 
+
     private int getMax() {
         int max = -Integer.MAX_VALUE;
         for(int i = 0; i < gdata.size(); i++) {
